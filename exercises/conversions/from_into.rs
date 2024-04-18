d7665ffbb146fb6bd3e127743a1903d4a575699a
luofu <mqa010225@163.com>
2024-04-18 16:08:39 +0800

    <feat>(24-spring-rustlings): <exercise>
    exercise:
      - tests
      - conv

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,20 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.is_empty() {
+            return Default::default();
+        }
+        match s.split_once(",") {
+            Some((name, _)) if name.is_empty() => Default::default(),
+            Some((name, age_str)) => match age_str.parse::<usize>() {
+                Ok(age) => Person { name: name.to_string(), age: age },
+                Err(_) => Default::default(),
+            }
+            None => Default::default(),
+        }
     }
 }
 
@@ -59,6 +69,7 @@ fn main() {
 #[cfg(test)]
 mod tests {
     use super::*;
+
     #[test]
     fn test_default() {
         // Test that the default person is 30 year old John
@@ -66,6 +77,7 @@ mod tests {
         assert_eq!(dp.name, "John");
         assert_eq!(dp.age, 30);
     }
+
     #[test]
     fn test_bad_convert() {
         // Test that John is returned when bad string is provided
@@ -73,6 +85,7 @@ mod tests {
         assert_eq!(p.name, "John");
         assert_eq!(p.age, 30);
     }
+
     #[test]
     fn test_good_convert() {
         // Test that "Mark,20" works
@@ -80,6 +93,7 @@ mod tests {
         assert_eq!(p.name, "Mark");
         assert_eq!(p.age, 20);
     }
+
     #[test]
     fn test_bad_age() {
         // Test that "Mark,twenty" will return the default person due to an
